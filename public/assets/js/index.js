a302bf044ca658fa9b7e5e9948a91a2ad60b9aec
“noarrassam” <noor.s.rassam@gmail.com>
2022-05-05 13:35:49 -0400

    first commit

---
+++ public/assets/js/index.js
@@ -0,0 +1,38 @@
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+// Show an element
+const show = (elem) => {
+  elem.style.display = 'inline';
+};
+
+// Hide an element
+const hide = (elem) => {
+  elem.style.display = 'none';
+};
+
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
+
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+
+//Delete Note
+const deleteNote = (id) => ({});
---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
