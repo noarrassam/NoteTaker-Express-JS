a01ff91d774827d86f91ce58f38c4b72a56c72cf
“noarrassam” <noor.s.rassam@gmail.com>
2022-05-07 17:39:29 -0400

    created post and delete methods

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const db = require("../../db/db.json");
+const uuid = require("uuid");
 
 router
   .route("/")
@@ -7,11 +8,34 @@ router
     res.json(db);
   })
   .post((req, res) => {
-    res.send(req.body);
+    const notes = {
+      uuid: uuid.v4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    if (!notes.title || !notes.text) {
+      return res.status(400).json({ msg: "Please include a title and text" });
+    }
+
+    db.push(notes);
+    res.json(db);
   });
 
+// Delete Note
 router.route("/:id").delete((req, res) => {
-  res.send(req.params.id);
+  const found = db.some((note) => note.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "Note deleted",
+      Notes: db.filter((note) => note.id !== parseInt(req.params.id)),
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ msg: `No Note with the ID of ${req.params.id} ` });
+  }
 });
 
 module.exports = router;
